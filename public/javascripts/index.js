8152da78574b1ea9f99893064527a6b442120f25
Liveipool <123653079@qq.com>
2017-02-16 16:36:07 +0800

    用JS实现三列高度动态变化并一致

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,6 +1,73 @@
-// $(document).ready(function() {
-// 	console.log("llalalal");
-// })
 window.onload = function() {
-	// console.log("haha")
+  var introduction=document.getElementById("introduction") 
+  var skill=document.getElementById("skill")
+  var experience=document.getElementById("experience") 
+
+  var introductionHeight = introduction.scrollHeight;
+  var skillHeight = skill.scrollHeight;
+  var experienceHeight = experience.scrollHeight;
+
+  var layoutHeight, display;
+
+  function initial() {
+    if (window.innerWidth >=1200) {
+      display = 3;
+      layoutHeight = Math.max(introductionHeight, skillHeight, experienceHeight) 
+      introduction.style.height = layoutHeight + 'px';
+      skill.style.height = layoutHeight + 'px';
+      experience.style.height = layoutHeight + 'px';
+    } else if (window.innerWidth < 1200 & window.innerWidth >= 768) {
+      display = 2;
+      layoutHeight = Math.max(introductionHeight, skillHeight) 
+      introduction.style.height = layoutHeight + 'px';
+      skill.style.height = layoutHeight + 'px';
+    } else {
+      display = 1;
+    }
+  }
+
+  initial();
+
+  var handler = function() {
+    if (window.innerWidth >= 1200) {
+      if (display != 3) { 
+        introduction.style.height = '';
+        skill.style.height = '';
+        experience.style.height = '';
+        display = 3;
+      }
+      if (window.innerWidth > 1500) {
+        introduction.style.height = '';
+        skill.style.height = '';
+        experience.style.height = '';        
+      }
+      introductionHeight = introduction.scrollHeight;
+      skillHeight = skill.scrollHeight;
+      experienceHeight = experience.scrollHeight;
+      layoutHeight=Math.max(introductionHeight, skillHeight, experienceHeight) 
+      introduction.style.height = layoutHeight + 'px';
+      skill.style.height = layoutHeight + 'px';
+      experience.style.height = layoutHeight + 'px';
+    } else if (window.innerWidth < 1200 & window.innerWidth >= 768) {
+      if (display != 2) { 
+        introduction.style.height = '';
+        skill.style.height = '';
+        experience.style.height = '';
+        display = 2;
+      }
+      introductionHeight = introduction.scrollHeight;
+      skillHeight = skill.scrollHeight;
+      layoutHeight=Math.max(introductionHeight, skillHeight) 
+      introduction.style.height = layoutHeight + 'px';
+      skill.style.height = layoutHeight + 'px';
+      experience.style.height = '';      
+    } else {
+      display = 1;
+      introduction.style.height = '';
+      skill.style.height = '';
+      experience.style.height = '';
+    }
+  }
+
+  window.onresize = handler;
 }
