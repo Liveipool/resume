5e00b553c6bf78d36fbdf676c8d3c037efe4493b
Liveipool <123653079@qq.com>
2017-02-21 01:30:08 +0800

    fix all bugs

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -13,23 +13,42 @@ window.onload = function() {
     skillHeight = skill.scrollHeight;
   }
 
-  function initial() {
-    // if (window.innerWidth >= 1440) {
-    //   introduction.className = 'part col-md-3 col-sm-4 col-xs-12 col-lg-2';
-    //   experience.className = 'part col-md-9 col-sm-8 col-xs-12 col-lg-6';
-    //   skill.className = 'part col-md-12 col-sm-12 col-xs-12 col-lg-4';
-    // } else if (window.innerWidth < 1440 && window.innerWidth >= 1200) {
-    //   introduction.className = 'part col-md-3 col-sm-4 col-xs-12 col-lg-4';
-    //   experience.className = 'part col-md-9 col-sm-8 col-xs-12 col-lg-8';
-    //   skill.className = 'part col-md-12 col-sm-12 col-xs-12 col-lg-12';
-    // }
+  function changeRate(isBiggerthan1440) {
+    if (isBiggerthan1440) {
+      introduction.className = 'part col-md-3 col-sm-4 col-xs-12 col-lg-2';
+      experience.className = 'part col-md-9 col-sm-8 col-xs-12 col-lg-6';
+      skill.className = 'part col-md-12 col-sm-12 col-xs-12 col-lg-4';
+      display = 3;     
+    } else {
+      introduction.className = 'part col-md-3 col-sm-4 col-xs-12 col-lg-3';
+      experience.className = 'part col-md-9 col-sm-8 col-xs-12 col-lg-9';
+      skill.className = 'part col-md-12 col-sm-12 col-xs-12 col-lg-12';
+      display = 2;      
+    }
+  }
 
-    if (window.innerWidth >= 1200) {
+  function assign(isIntroProvided, isExperProvided, isSkillProvided) {
+    introduction.style.height = isIntroProvided == true ? layoutHeight + 'px' : '';
+    experience.style.height = isExperProvided == true ? layoutHeight + 'px' : '';
+    skill.style.height = isSkillProvided == true ? layoutHeight + 'px' : '';
+  }
+
+  function initial() {
+    if (window.innerWidth >= 1440) {
+      changeRate(true);
+    } else if (window.innerWidth < 1440 && window.innerWidth >= 1200) {
+      console.log()
+      changeRate(false);
+    }
+
+    if (window.innerWidth >= 1440) {
       display = 3;
+      getScrollHeight();
       layoutHeight = Math.max(introductionHeight, experienceHeight, skillHeight) 
       assign(true, true, true);
-    } else if (window.innerWidth < 1200 & window.innerWidth >= 768) {
+    } else if (window.innerWidth < 1440 & window.innerWidth >= 768) {
       display = 2;
+      getScrollHeight();
       layoutHeight = Math.max(introductionHeight, experienceHeight) 
       assign(true, true, false);
     } else {
@@ -39,15 +58,11 @@ window.onload = function() {
 
   initial();
 
-  function assign(isIntroProvided, isExperProvided, isSkillProvided) {
-    introduction.style.height = isIntroProvided == true ? layoutHeight + 'px' : '';
-    experience.style.height = isExperProvided == true ? layoutHeight + 'px' : '';
-    skill.style.height = isSkillProvided == true ? layoutHeight + 'px' : '';
-  }
-
   var handler = function() {
-    if (window.innerWidth >= 1200) {
+    console.log(window.innerWidth);
+    if (window.innerWidth >= 1440) {
       if (display != 3) { 
+        changeRate(true);
         assign(false, false, false);
         display = 3;
       }
@@ -57,7 +72,13 @@ window.onload = function() {
       getScrollHeight();
       layoutHeight=Math.max(introductionHeight, experienceHeight, skillHeight) 
       assign(true, true, true);
-    } else if (window.innerWidth < 1200 & window.innerWidth >= 768) {
+    } else if (window.innerWidth < 1440 & window.innerWidth >= 768) {
+      if (window.innerWidth >= 1200) {
+        changeRate(false);
+      }
+      if (window.innerWidth > 800) {
+        assign(false, false, false);
+      }
       if (display != 2) { 
         assign(false, false, false);
         display = 2;
